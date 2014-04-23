95d0304d9ed70766a6acaab135f1013cbe7a67a4
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2014-04-23 15:32:33 +0200

    added 1.2 versjon for merging csv coulumns

---
+++ mergetwofirst.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python                                                           
+# -*- coding: utf-8 -*-
+#
+# Merge first two colums in csv file
+# Tested onnnnn python3
+#
+# @author Per-Henrik E. Kvalnes
+#
+import sys
+
+splittSym = ";"
+
+# Open files 
+filename = sys.argv[1]
+
+try:
+        infile = open(filename)
+except:
+        print("Kan ikke aapne filen " + filename)
+
+outfile = open(filename+"New","w")
+
+
+# Travaser csv filen 
+for line in infile:
+    # fjern linje skift
+    line = line.rstrip() 
+    words = line.split(splittSym) 
+
+    # hvis det er innhold i kollone 2, ta det med.
+    if(words[1] != ""):
+        newline = words[0]+" "+words[1]+";"
+    else:
+        newline = words[0]+";"
+
+    i = 2
+    while i < len(words):
+        newline += words[i]+";"
+        i += 1
+
+        # Legg til nytt linjeskift der det skal vare
+    newline += "\n"
+    outfile.write(newline)
+
+
+infile.close()
+outfile.close()
+
+print("New file has been written to "+filename+"New.csv")
